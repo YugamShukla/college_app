0ebd3f06c88716601f228e5d47b3b74d29360b52
Chirag Agarwal <43610625+Chiragga24@users.noreply.github.com>
2020-12-21 19:23:30 +0530

    Create chart_test.dart
    For pie chart, yvalues should all add up to 100 (percentages) and should be in float

---
+++ chart_test.dart
@@ -0,0 +1,305 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:random_color/random_color.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: ChartBuilder(),
+    );
+  }
+}
+
+class ChartBuilder extends StatefulWidget {
+  ChartBuilder({this.xvalues,this.yvalues,this.graph});
+  List xvalues;
+  List yvalues;
+  int graph;
+  @override
+  _ChartBuilderState createState() {
+    return _ChartBuilderState("ABC","ABC",[1,2,3,4],[25.0,25.0,25.0,25.0],0);
+  }
+}
+
+class _ChartBuilderState extends State<ChartBuilder> {
+  List xvalues;
+  List yvalues;
+  int graph;
+  String xlabel;
+  String ylabel;
+  _ChartBuilderState(this.xlabel,this.ylabel,this.xvalues,this.yvalues,this.graph);
+  List<charts.Series<LineChartValue, int>> _seriesLineData;
+  List<charts.Series<BarChartValue, String>> _seriesBarData;
+  List<charts.Series<PieChartValue, String>> _seriesPieData;
+  RandomColor _randomColor = RandomColor();
+  _generatelineData(){
+    var linedata = [
+      for(int i=0;i<xvalues.length;i++)
+        new LineChartValue(xvalues[i], yvalues[i])
+    ];
+    _seriesLineData.add(
+      charts.Series(
+        colorFn: (__, _) => charts.ColorUtil.fromDartColor(Color(0xff990099)),
+        id: 'Line Graph',
+        data: linedata,
+        domainFn: (LineChartValue chart, _) => chart.xval,
+        measureFn: (LineChartValue chart, _) => chart.yval,
+      ),
+    );
+  }
+  _generatebarData(){
+    var bar = [
+      for(int i=0;i<xvalues.length;i++)
+        new BarChartValue(xvalues[i].toString(), yvalues[i])
+    ];
+    _seriesBarData.add(
+      charts.Series(
+        domainFn: (BarChartValue chart, _) => chart.year,
+        measureFn: (BarChartValue chart, _) => chart.yval,
+        id: 'Bar Graph',
+        data: bar,
+        fillPatternFn: (_, __) => charts.FillPatternType.solid,
+        fillColorFn: (BarChartValue chart, _) =>
+            charts.ColorUtil.fromDartColor(Color(0xff990099)),
+      ),
+    );
+  }
+  _generatepiedata(){
+    var pie = [
+      for(int i=0;i<xvalues.length;i++)
+        new PieChartValue(xvalues[i].toString(), yvalues[i], _randomColor.randomColor())
+    ];
+    _seriesPieData.add(
+      charts.Series(
+        domainFn: (PieChartValue task, _) => task.task,
+        measureFn: (PieChartValue task, _) => task.taskvalue,
+        colorFn: (PieChartValue task, _) =>
+            charts.ColorUtil.fromDartColor(task.colorval),
+        id: 'Air Pollution',
+        data: pie,
+        labelAccessorFn: (PieChartValue row, _) => '${row.taskvalue}',
+      ),
+    );
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    _seriesLineData = List<charts.Series<LineChartValue, int>>();
+    _seriesBarData = List<charts.Series<BarChartValue, String>>();
+    _seriesPieData = List<charts.Series<PieChartValue, String>>();
+    if(graph == 2)
+      _generatelineData();
+    else if(graph == 1)
+      _generatebarData();
+    else if(graph ==0)
+      _generatepiedata();
+  }
+  Material mylineChart(String title, int color) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      shadowColor: Color(0x802196F3),
+      borderRadius: BorderRadius.circular(24.0),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    title,
+                    style: TextStyle(color: new Color(color), fontSize: 20.0),
+                  ),
+                ),
+                SizedBox(
+                    width: 350,
+                    height: 350,
+                    child: charts.LineChart(
+                        _seriesLineData,
+                        defaultRenderer: new charts.LineRendererConfig(
+                            includeArea: true, stacked: true),
+                        animate: true,
+                        animationDuration: Duration(seconds: 3),
+                        behaviors: [
+                          new charts.ChartTitle(xlabel,
+                              behaviorPosition: charts.BehaviorPosition.bottom,
+                              titleOutsideJustification: charts
+                                  .OutsideJustification.middleDrawArea),
+                          new charts.ChartTitle(ylabel,
+                              behaviorPosition: charts.BehaviorPosition.start,
+                              titleOutsideJustification: charts
+                                  .OutsideJustification.middleDrawArea),
+                        ]
+                    )
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+    Material myBarChart(String title, int color) {
+      return Material(
+        color: Colors.white,
+        elevation: 14.0,
+        shadowColor: Color(0x802196F3),
+        borderRadius: BorderRadius.circular(24.0),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(color: new Color(color), fontSize: 20.0),
+                    ),
+                  ),
+                  SizedBox(
+                      width: 350,
+                      height: 350,
+                      child: charts.BarChart(
+                        _seriesBarData,
+                        animate: true,
+                          behaviors: [
+                            new charts.ChartTitle(xlabel,
+                                behaviorPosition: charts.BehaviorPosition.bottom,
+                                titleOutsideJustification: charts
+                                    .OutsideJustification.middleDrawArea),
+                            new charts.ChartTitle(ylabel,
+                                behaviorPosition: charts.BehaviorPosition.start,
+                                titleOutsideJustification: charts
+                                    .OutsideJustification.middleDrawArea),
+                          ],
+                        animationDuration: Duration(seconds: 5),
+                      ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+  }
+  Material myPieChart(String title, int color) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      shadowColor: Color(0x802196F3),
+      borderRadius: BorderRadius.circular(24.0),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    title,
+                    style: TextStyle(color: new Color(color), fontSize: 20.0),
+                  ),
+                ),
+                SizedBox(
+                    width: 350,
+                    height: 350,
+                    child: charts.PieChart(
+                      _seriesPieData,
+                      animate: true,
+                      animationDuration: Duration(seconds: 5),
+                      behaviors: [
+                    new charts.DatumLegend(
+                    outsideJustification: charts.OutsideJustification.endDrawArea,
+                      horizontalFirst: false,
+                      desiredMaxRows: 2,
+                      cellPadding: new EdgeInsets.only(right: 4.0, bottom: 4.0),
+                      entryTextStyle: charts.TextStyleSpec(
+                          color: charts.MaterialPalette.purple.shadeDefault,
+                          fontFamily: 'Georgia',
+                          fontSize: 11),
+                    )
+                      ],
+                        defaultRenderer: new charts.ArcRendererConfig(
+                            arcWidth: 100,
+                            arcRendererDecorators: [
+                              new charts.ArcLabelDecorator(
+                                  labelPosition: charts.ArcLabelPosition.inside)
+                            ])),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: StaggeredGridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12.0,
+        mainAxisSpacing: 12.0,
+        padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+        children: <Widget>
+        [graph==1? myBarChart( "Faculty Awards", 0xffed622b):myPieChart("Faculty Awards", 0xffed622b),
+        ],
+        staggeredTiles: [
+          StaggeredTile.extent(2, 400.0),
+        ],
+      ),
+    );
+  }
+}
+
+class LineChartValue{
+  int xval;
+  int yval;
+
+  LineChartValue(this.xval,this.yval);
+}
+class BarChartValue{
+  String year;
+  int yval;
+  BarChartValue(this.year,this.yval);
+}
+
+class PieChartValue {
+  String task;
+  double taskvalue;
+  Color colorval;
+
+  PieChartValue(this.task, this.taskvalue, this.colorval);
+}
